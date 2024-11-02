d2acb32b4036eaec8b23b32d971865d1c0e3a073
Rorisang Putu <rorisangputu@gmail.com>
2024-11-02 03:32:12 +0200

    creating new component

---
+++ app/components/Work/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Work.modules.scss';
+const Work = () => {
+    return (
+        <div className={styles.workContainer}>
+
+        </div>
+    )
+}
+
+export default Work
