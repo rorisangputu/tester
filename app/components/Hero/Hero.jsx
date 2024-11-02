3e2702b435078416fc9a00f698c0b666de57f799
Rorisang Putu <rorisangputu@gmail.com>
2024-11-02 03:23:17 +0200

    adding div

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -11,41 +11,44 @@ const Hero = ({ pageLoaded }) => {
 
     return (
 
-        <div className={styles.header}>
-            <header className={styles.hero} ref={heroRef}>
-                <div className={styles.nav}>
-                    <p data-nav-items className={styles.logo}>
-                        LAIOTIX
-                    </p>
-                    <div data-nav-items className={styles.contacts}>
-                        <p>+27 (0)79 863 0810</p>
-                        <p>info@laiotix.com</p>
-                    </div>
-                </div>
-                <div className={styles.title}>
-                    <p data-title-first className={styles.heroTitle}>
-                        LAIOTIX
-                    </p>
-                    <p data-title-second data-hidden className={styles.heroBound}>INBOUND</p>
-                </div>
-
-                <div className={styles.subcontainer}>
-                    <div className={styles.subtitle}>
-                        <p data-items>*</p>
-                        <p data-items className={styles.subtext}>
-                            Currently Redefining
-                            <span data-items className={styles.subtextBlock}>
-                                the Future of Design
-                            </span>
+        <div>
+            <div className={styles.header}>
+                <header className={styles.hero} ref={heroRef}>
+                    <div className={styles.nav}>
+                        <p data-nav-items className={styles.logo}>
+                            LAIOTIX
                         </p>
+                        <div data-nav-items className={styles.contacts}>
+                            <p>+27 (0)79 863 0810</p>
+                            <p>info@laiotix.com</p>
+                        </div>
                     </div>
-                    <div data-items className={styles.scroll}>
-                        <GoArrowDownRight className={styles.scrollarrow} />
-                        <p className={styles.scrolltext}>Scroll</p>
+                    <div className={styles.title}>
+                        <p data-title-first className={styles.heroTitle}>
+                            LAIOTIX
+                        </p>
+                        <p data-title-second data-hidden className={styles.heroBound}>INBOUND</p>
                     </div>
-                </div>
-            </header>
-            <Lox />
+
+                    <div className={styles.subcontainer}>
+                        <div className={styles.subtitle}>
+                            <p data-items>*</p>
+                            <p data-items className={styles.subtext}>
+                                Currently Redefining
+                                <span data-items className={styles.subtextBlock}>
+                                    the Future of Design
+                                </span>
+                            </p>
+                        </div>
+                        <div data-items className={styles.scroll}>
+                            <GoArrowDownRight className={styles.scrollarrow} />
+                            <p className={styles.scrolltext}>Scroll</p>
+                        </div>
+                    </div>
+                </header>
+                <Lox />
+            </div>
+            
         </div>
 
     );
