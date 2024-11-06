ef80ae95f0a0be3e3773e065a172f000d004ebd5
Rorisang Putu <rorisangputu@gmail.com>
2024-11-06 13:16:57 +0200

    commit

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import styles from "./Footer.module.css";
+import { GoArrowUpLeft } from "react-icons/go";
+
 const Footer = () => {
     return (
-        <div className={styles.FooterContainer}>
-            <div>
-                <div>
-                    <p>Let&apos;s talk</p>
-                    <div data-nav-items className={styles.contacts}>
+        <div className={styles.footerContainer}>
+            <div className={styles.textContainer}>
+                <div className={styles.upperText}>
+                    <p className={styles.Talk}>Let&apos;s talk</p>
+                    <div className={styles.contacts}>
                         <p>+27 (0)79 863 0810</p>
                         <p>info@laiotix.com</p>
                     </div>
                 </div>
-                <div>
-
+                <div className={styles.lowerText}>
+                    <div className={styles.footerText}>
+                        <p>LAIOTIX</p>
+                    </div>
+                    <div className={styles.lowerFooterText}>
+                        <div className={styles.backToTop}>
+                            <GoArrowUpLeft className={styles.topArrow} />
+                            <p>Back to top</p>
+                        </div>
+                        <div className={styles.copyright}>
+                            <p className={styles.copycopy}>Copyright</p>
+                            <p>&copy; Laiotix 2024</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div >
--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -6,6 +6,7 @@ import { revealText } from "./animations";
 import Lox from "../Lox/Lox";
 import Work from "../Work/Work";
 import Footer from "../Footer/Footer";
+import Image from "next/image";
 
 const Hero = ({ pageLoaded }) => {
     const heroRef = useRef(null);
@@ -29,7 +30,26 @@ const Hero = ({ pageLoaded }) => {
                         <p data-title-first className={styles.heroTitle}>
                             LAIOTIX
                         </p>
-                        <p data-title-second className={styles.heroBound}>INBOUND</p>
+                        <div className={styles.hoverDiv}>
+                            <p data-title-second className={styles.heroBound}>INBOUND</p>
+                            {/* <div className={styles.hoverImages}>
+                                <div className={`${styles.imageContainers} ${styles.firstImg}`}>
+                                    <Image src="https://images.unsplash.com/photo-1600191763994-a5e80844476e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" fill objectFit="cover" />
+                                </div>
+                                <div className={`${styles.imageContainers} ${styles.sndImg}`}>
+                                    <Image src="https://images.unsplash.com/photo-1600191763994-a5e80844476e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" fill objectFit="cover" />
+                                </div>
+                                <div className={`${styles.imageContainers} ${styles.thirdImg}`}>
+                                    <Image src="https://images.unsplash.com/photo-1600191763994-a5e80844476e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" fill objectFit="cover" />
+                                </div>
+                                <div className={`${styles.imageContainers} ${styles.fourthImg}`}>
+                                    <Image src="https://images.unsplash.com/photo-1600191763994-a5e80844476e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" fill objectFit="cover" />
+                                </div>
+                                <div className={`${styles.imageContainers} ${styles.fifthImg}`}>
+                                    <Image src="https://images.unsplash.com/photo-1600191763994-a5e80844476e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image" fill objectFit="cover" />
+                                </div>
+                            </div> */}
+                        </div>
                     </div>
 
                     <div className={styles.subcontainer}>
